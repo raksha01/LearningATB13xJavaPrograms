1149e23bce436a8af43e37cf951fabdda04cb6dd
raksha <rakshacs0033@gmail.com>
2025-06-07 19:51:12 +0530

    Commit HackerRank Exercise 2

---
+++ src/ex_01_Java_Basic/hackerRank2.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basic;
+
+import java.util.Scanner;
+
+public class hackerRank2
+{
+   public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+            // Complete this line
+            // Complete this line
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
+
+
+}
