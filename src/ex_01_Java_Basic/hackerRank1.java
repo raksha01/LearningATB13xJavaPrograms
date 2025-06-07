fdec9aaf7787534e6fbbaeb887602e5738a897e6
raksha <rakshacs0033@gmail.com>
2025-06-07 19:47:52 +0530

    Commit HackerRank Exercise 1

---
+++ src/ex_01_Java_Basic/hackerRank1.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class hackerRank1 {
+    public static void main(String[] args) {
+           System.out.println("Hello, World.\nHello, Java.");
+            // System.out.println("Hello, Java.");
+        }
+    }
+
+}
