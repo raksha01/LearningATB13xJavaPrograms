b4f7f33b1cdc3b61bba7caaf145105ce5bebc2f0
raksha <rakshacs0033@gmail.com>
2025-06-10 22:02:01 +0530

    Commit Table of 9

---
+++ src/ex_01_Java_Basic/tableOfNine.java
@@ -0,0 +1,28 @@
+package ex_01_Java_Basic;
+
+public class tableOfNine {
+    public static void main(String[] args) {
+        int a = 9, b = 1;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+
+
+    }
+}
