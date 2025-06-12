9106b489625e2d3204f60997bc8d29d8a551ed44
raksha <rakshacs0033@gmail.com>
2025-06-12 20:09:47 +0530

    ternary operator for even/odd number

---
+++ src/ex_01_Java_Basic/ternaryEvenNumber.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class ternaryEvenNumber {
+    public static void main(String[] args) {
+        int a = 20;
+        String result = (a%2==0)?"It isn even number":"It is an odd number";
+        System.out.println(result);
+    }
+}
