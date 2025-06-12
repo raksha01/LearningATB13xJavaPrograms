753887b5236dc2e9ee5c586c27dfd5c29371c882
raksha <rakshacs0033@gmail.com>
2025-06-12 23:03:17 +0530

    Various DataTypes

--- src/typecasting/typecastingCharToInt.java
+++ src/typecasting/typecastingCharToInt.java
@@ -4,9 +4,9 @@ public class typecastingCharToInt {
     public static void main(String[] args) {
         char c='Z';
         int a = c;
-        System.out.println(a);
-        int i1 = 74;
+        System.out.println(a); // Printing the ASCII value
+        int i1 = 174;
         char c1=(char) i1;
-        System.out.println(c1);
+        System.out.println(c1); // Updating the ASCII value to char
     }
 }
