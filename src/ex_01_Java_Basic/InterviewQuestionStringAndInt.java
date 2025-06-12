7cc5de97cfbf62fad675e8e51920a99437ee25a0
raksha <rakshacs0033@gmail.com>
2025-06-12 23:33:11 +0530

    Interview Question

---
+++ src/ex_01_Java_Basic/InterviewQuestionStringAndInt.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
+public class InterviewQuestionStringAndInt {
+    public static void main(String[] args) {
+        System.out.println("Test"+"User"); //TestUser
+        System.out.println("Test"+10); //Test10
+        System.out.println("Test"+10+20); //Test1020
+        System.out.println(10+"Test"); //10Test
+        System.out.println(10+20+"Test"); //30Test
+        System.out.println(10+20+"Test"+10+20); //30Test1020
+    }
+}
