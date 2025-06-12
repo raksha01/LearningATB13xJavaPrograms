19f6eff3b45f6b99d1c0d4b2da04344b65b158f6
raksha <rakshacs0033@gmail.com>
2025-06-12 22:36:53 +0530

    Various DataTypes

--- src/ex_01_Java_Basic/dataTypes.java
+++ src/ex_01_Java_Basic/dataTypes.java
@@ -9,6 +9,7 @@ public class dataTypes {
         float f = 115357000961111111111111111111116535700.022222f;
         double d = 15357000961111111111111111111116535700.0221222334222222;
         char c = 'a';
-        System.out.println("Int : "+i+"\nShort : "+s+"\nByte : "+b+"\nLong : "+l+"\nFloat : "+f+"\nDecimal : "+d+"\nChar : "+c);
+        boolean bl = false;
+        System.out.println("Int : "+i+"\nShort : "+s+"\nByte : "+b+"\nLong : "+l+"\nFloat : "+f+"\nDecimal : "+d+"\nChar : "+c+"\nBoolean : "+bl);
     }
 }
