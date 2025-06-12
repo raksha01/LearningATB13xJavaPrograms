ce2273df89c9e2032509bd72caa8a63a208d8dc3
raksha <rakshacs0033@gmail.com>
2025-06-12 22:59:23 +0530

    Various DataTypes

--- src/typecasting/typecastingCharToInt.java
+++ src/typecasting/typecastingCharToInt.java
@@ -1,9 +1,12 @@
 package typecasting;
 
-public class typecastingChatToInt {
+public class typecastingCharToInt {
     public static void main(String[] args) {
         char c='A';
         int a = (int)c;
         System.out.println(a);
+        int i1 = 74;
+        char c1=(char) i1;
+        System.out.println(c1);
     }
 }
