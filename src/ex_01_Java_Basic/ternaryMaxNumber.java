c1c22919197fc850ee5aa8da94086d3db029cd43
raksha <rakshacs0033@gmail.com>
2025-06-12 20:03:58 +0530

    ternary operator for max number

---
+++ src/ex_01_Java_Basic/ternaryMaxNumber.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class ternaryMaxNumber {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 20;
+        int c = a>b?a:b;
+        System.out.println(c);
+    }
+}
