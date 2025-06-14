59cd576ab5c69b06d158815b3841e285e735414e
raksha <rakshacs0033@gmail.com>
2025-06-14 23:52:18 +0530

    hacker rank 3

---
+++ src/ex_01_Java_Basic/hackerRank4.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+import java.util.*;
+
+public class hackerRank4 {
+
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            // Complete this line
+            // Complete this line
+
+            System.out.println(a);
+            // Complete this line
+            // Complete this line
+        }
+    }
+}
