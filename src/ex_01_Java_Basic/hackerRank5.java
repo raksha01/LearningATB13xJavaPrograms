aac9b7e6c0e408fabad75a7e8bccfa53580a7007
raksha <rakshacs0033@gmail.com>
2025-06-14 23:57:44 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_01_Java_Basic/hackerRank5.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+import java.util.Scanner;
+
+public class hackerRank5 {
+
+   public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int i = scan.nextInt();
+
+            // Write your code here.
+
+            System.out.println("String: " + s);
+            System.out.println("Double: " + d);
+            System.out.println("Int: " + i);
+        }
+
+}
